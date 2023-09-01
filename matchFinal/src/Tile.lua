685cbc6b1297243b01dc832ef69407be63a2e351
Ian Skelskey <ianskelskey@gmail.com>
2023-08-31 23:07:14 -0400

    Add shiny visuals to some tiles

--- matchFinal/src/Tile.lua
+++ matchFinal/src/Tile.lua
@@ -9,9 +9,7 @@
 
     The individual tiles that make up our game board. Each Tile can have a
     color and a variety, with the varietes adding extra points to the matches.
-]]
-
-Tile = Class{}
+]] Tile = Class {}
 
 function Tile:init(x, y, color, variety)
     -- board positions
@@ -25,6 +23,7 @@ function Tile:init(x, y, color, variety)
     -- tile appearance/points
     self.color = color
     self.variety = variety
+    self.shiny = math.random(1, 20) == 1 and true or false
 end
 
 function Tile:update(dt)
@@ -40,12 +39,24 @@ end
 
 function Tile:render(x, y)
     -- draw shadow
-    love.graphics.setColor(34, 32, 52, 255)
-    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
-        self.x + x + 2, self.y + y + 2)
+    love.graphics.setColor(34/255, 32/255, 52/255, 1)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety], self.x + x + 2, self.y + y + 2)
 
     -- draw tile itself
-    love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
-        self.x + x, self.y + y)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety], self.x + x, self.y + y)
+
+    -- draw shiny effect
+    if self.shiny then
+        -- Use 'add' blend mode to make it more visible
+        love.graphics.setBlendMode('add')
+        -- This changes the color to a light yellow
+        love.graphics.setColor(1, 1, 1, 1)
+        local alpha = 96 + math.sin(love.timer.getTime() * 4) * 64
+        love.graphics.setColor(1, 1, 1, alpha/255)
+        love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety], self.x + x, self.y + y)
+    
+        love.graphics.setBlendMode('alpha')
+        love.graphics.setColor(1, 1, 1, 1)
+    end
 end
